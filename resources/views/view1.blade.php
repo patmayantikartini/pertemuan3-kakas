02d1ad283446909bbadb73782b0fbcdf48a3067b
Patmayanti <60900119027@uin-alauddin.ac.id>
2021-10-26 11:32:00 +0800

    Selamat Pagi

---
+++ resources/views/view1.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Patmayanti Kartini_60900119027</title>
+    </head>
+<body>
+    <body style="background-color:rgb(219, 209, 174)"> </body>
+    <center>
+    <h1>Data Diri</h1>
+    <p> Nama 				    : Patmayanti Kartini</br>
+    <p> NIM                     : 60900119027
+    <p> Kelas                   : B
+    <p> Emain                	: 60900119027@uin-alauddin.ac.id		
+</body>
+</html>
