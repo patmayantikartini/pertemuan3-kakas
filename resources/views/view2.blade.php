b4bf978df36028a864b760729cc5031144dfda42
Patmayanti <60900119027@uin-alauddin.ac.id>
2021-10-26 11:37:29 +0800

    Selamat Siang

---
+++ resources/views/view2.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Patmayanti Kartini_60900119027</title>
+    </head>
+<body>
+    <body style="background-color:rgb(219, 209, 174)"> </body>
+    <center>
+    <h1>Alasan Belajar Kakas</h1>
+    <p> Alasan saya belajar kakas untuk mempelajari tentang pengelolaan suatu kode</p>	
+</body>
+</html>
